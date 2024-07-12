326c0406e2692ea83c5bca2bda924ee5021bf4a1
Akshay Goyal <akshay.goyal1008@gmail.com>
2024-07-12 16:59:37 +0530

    Implement StringCalculator class with add method, handle empty string, basic numbers, new lines, custom delimiters, and negative number handling in tests

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,25 +1,42 @@
 require_relative '../string_calculator'
 
-RSpec.describe "StringCalculator" do
-  describe "#add" do
-    it "returns 0 for an empty string" do
-      expect(add("")).to eq(0)
-    end
-
-    it "returns the number itself for a single number" do
-      expect(add("1")).to eq(1)
-    end
-
-    it "returns the sum of two numbers" do
-      expect(add("1,5")).to eq(6)
-    end
-
-    it "handles new lines between numbers" do
-      expect(add("1\n2,3")).to eq(6)
+RSpec.describe StringCalculator do
+  describe ".add" do
+    it "returns 0 for empty string" do
+      expect(StringCalculator.add("")).to eq(0)
     end
     
-    it "supports different delimiters" do
-      expect(add("//;\n1;2")).to eq(3)
+    it "returns the sum of numbers in a comma-separated string" do
+      expect(StringCalculator.add("1,2,3")).to eq(6)
     end
+    
+    it "returns the sum of numbers with new lines and commas" do
+      expect(StringCalculator.add("1\n2,3\n4")).to eq(10)
+    end
+    
+    it "handles custom delimiters" do
+      expect(StringCalculator.add("//;\n1;2;3")).to eq(6)
+    end
+
+    it "raises an exception for negative numbers" do
+      expect { StringCalculator.add("-1,2,-3") }.to raise_error("negatives not allowed: -1, -3")
+    end
+
+    it "ignores numbers larger than 1000" do
+      expect(StringCalculator.add("2,1001,6")).to eq(8)
+    end
+
+    it "handles a mix of scenarios" do
+      expect(StringCalculator.add("1\n2,3\n1001,4")).to eq(10)
+    end
+
+    it "supports multiple delimiters with different lengths" do
+      expect(StringCalculator.add("//[abc][def]\n1abc2def3")).to eq(6)
+    end
+
+    it "supports multiple delimiters with different characters" do
+      expect(StringCalculator.add("//[x][y][z]\n1x2y3z4")).to eq(10)
+    end
+    
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,16 +1,59 @@
-def add(numbers)
+class StringCalculator
+  def self.add(numbers)
     return 0 if numbers.empty?
-    
-    delimiter = ","
-    if numbers.start_with?("//")
-      delimiter = numbers[2]
-      numbers = numbers[4..-1]
+
+    delimiter = find_delimiters(numbers)
+    numbers_without_delimiter = remove_delimiter_declaration(numbers)
+
+    nums = split_numbers(numbers_without_delimiter, delimiter).map(&:to_i)
+
+    negatives = nums.select { |num| num < 0 }
+    if negatives.any?
+      raise "negatives not allowed: #{negatives.join(', ')}"
     end
-    
-    numbers.split(/[,\n#{delimiter}]/).map(&:to_i).sum
+
+    sum = nums.inject(0) do |acc, num|
+      num <= 1000 ? acc + num : acc
+    end
+
+    sum
+  end
+
+  def self.find_delimiters(numbers)
+    if numbers.start_with?("//")
+      delimiter_line = numbers.lines.first.chomp
+      delimiter_line.gsub!("//", "")
+      delimiter_line.scan(/\[([^\[\]]+)\]/).flatten
+    else
+      [","]
+    end
+  end
+
+  def self.remove_delimiter_declaration(numbers)
+    if numbers.start_with?("//")
+      numbers.lines.drop(1).join
+    else
+      numbers
+    end
+  end
+
+  def self.split_numbers(numbers, delimiters)
+    delimiters_regex = delimiters.map { |delim| Regexp.escape(delim) }.join("|")
+    numbers.split(/#{delimiters_regex}|\n/)
+  end
 end
 
-puts "Enter numbers separated by commas or newlines:"
-input = gets.chomp
-result = add(input)
-puts "Sum: #{result}"
+puts StringCalculator.add("")                  # Output: 0
+puts StringCalculator.add("1")                 # Output: 1
+puts StringCalculator.add("1,2")               # Output: 3
+puts StringCalculator.add("1\n2,3")            # Output: 6
+puts StringCalculator.add("//;\n1;2")          # Output: 3 (delimiter is ';')
+puts StringCalculator.add("//***\n1***2***3")  # Output: 6 (delimiter is '***')
+puts StringCalculator.add("//[*][%]\n1*2%3")   # Output: 6 (delimiters are '*' and '%')
+puts StringCalculator.add("2,1001,6")          # Output: 8 (1001 is ignored)
+puts StringCalculator.add("//[***][%]\n1***2%3") # Output: 6 (delimiters are '***' and '%')
+begin
+  puts StringCalculator.add("-1,2,-3")         # Exception: negatives not allowed: -1, -3
+rescue => e
+  puts "Exception: #{e.message}"
+end
