f4bd35bd76164f5dc7da0dfe022337187cce6fe4
Akshay Goyal <akshay.goyal1008@gmail.com>
2024-07-12 11:37:09 +0530

    Extended functionality to handle new lines between numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,9 @@
 def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(",").map(&:to_i).sum
+    numbers.split(/[,\n]/).map(&:to_i).sum
 end
-  
+
+puts "Enter numbers separated by commas or newlines:"
+input = gets.chomp
+result = add(input)
+puts "Sum: #{result}"
