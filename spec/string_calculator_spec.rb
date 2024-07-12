f769bea822aeadccd4d3043e53d08332a62f65df
Akshay Goyal <akshay.goyal1008@gmail.com>
2024-07-12 10:55:44 +0530

    Implemented basic functionality to handle empty string and comma-separated numbers

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../string_calculator'
+
+RSpec.describe "StringCalculator" do
+  describe "#add" do
+    it "returns 0 for an empty string" do
+      expect(add("")).to eq(0)
+    end
+
+    it "returns the number itself for a single number" do
+      expect(add("1")).to eq(1)
+    end
+
+    it "returns the sum of two numbers" do
+      expect(add("1,5")).to eq(6)
+    end
+  end
+end
